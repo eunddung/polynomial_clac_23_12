da4ef5297ec0a96c33a9c64462844d1f189258c7
eunddung <x72845221@gmail.com>
2023-12-27 16:17:14 +0900

    식에서의 불필요한 괄호 제거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,17 +2,21 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+
+    exp = stripOuterBracket(exp);
+
     boolean needToMultiply = exp.contains(" * ");
     boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
 
     boolean needToCompound = needToMultiply && needToPlus;
-
     if (needToCompound) {
       String[] bits = exp.split(" \\+ ");
+
       return Integer.parseInt(bits[0]) + run(bits[1]);
     }
     if (needToPlus) {
       exp = exp.replaceAll("\\- ", "\\+ \\-");
+
       String[] bits = exp.split(" \\+ ");
       int sum = 0;
       for (int i = 0; i < bits.length; i++) {
@@ -27,6 +31,14 @@ public class Calc {
       }
       return rs;
     }
+
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+  }
+
+  private static String stripOuterBracket(String exp) {
+    if (exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')') {
+      exp = exp.substring(1, exp.length() - 1);
+    }
+    return exp;
   }
 }
