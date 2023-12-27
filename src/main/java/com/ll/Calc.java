34a50c2640940146df1bdb40531659d51ce41ec6
eunddung <x72845221@gmail.com>
2023-12-27 16:36:19 +0900

    여러겹의 괄호 제거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,21 +2,16 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
-
     exp = stripOuterBracket(exp);
-
     boolean needToMultiply = exp.contains(" * ");
     boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
-
     boolean needToCompound = needToMultiply && needToPlus;
     if (needToCompound) {
       String[] bits = exp.split(" \\+ ");
-
       return Integer.parseInt(bits[0]) + run(bits[1]);
     }
     if (needToPlus) {
       exp = exp.replaceAll("\\- ", "\\+ \\-");
-
       String[] bits = exp.split(" \\+ ");
       int sum = 0;
       for (int i = 0; i < bits.length; i++) {
@@ -31,14 +26,21 @@ public class Calc {
       }
       return rs;
     }
-
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 
   private static String stripOuterBracket(String exp) {
-    if (exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')') {
-      exp = exp.substring(1, exp.length() - 1);
+    int outerBracketCount = 0;
+
+    while (exp.charAt(outerBracketCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketCount) == ')') {
+      outerBracketCount++;
     }
-    return exp;
+
+    if (outerBracketCount == 0) {
+      return exp;
+    }
+
+    ;
+    return exp.substring(outerBracketCount, exp.length() - outerBracketCount);
   }
 }
