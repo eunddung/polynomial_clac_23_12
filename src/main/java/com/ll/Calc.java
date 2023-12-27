9cb64dee7b22265e654b74ac29f72213dab5b3fb
eunddung <x72845221@gmail.com>
2023-12-27 09:35:10 +0900

    ëº뺼셈 ê¸빼기기능구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,27 +3,26 @@ package com.ll;
 public class Calc {
   public static int run(String exp) {
 
+    boolean needToPlus = exp.contains("+");
+    boolean needToMinus = exp.contains("-");
 
-    String[] bits = exp.split(" \\+ ");
+    String[] bits = null;
 
-
-    int a = Integer.parseInt(bits[0]);
-    int b = Integer.parseInt(bits[1]);
-
-    return a + b;
-
-
-  }
-
-  public static int run2(String exp) {
-
-
-    String[] bits = exp.split(" \\- ");
-
+    if (needToPlus) {
+      bits = exp.split(" \\+ ");
+    } else if (needToMinus) {
+      bits = exp.split(" - ");
+    }
 
     int a = Integer.parseInt(bits[0]);
     int b = Integer.parseInt(bits[1]);
 
-    return a - b;
+    if (needToPlus) {
+      return a + b;
+    } else if (needToMinus) {
+      return a - b;
+    }
+
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
