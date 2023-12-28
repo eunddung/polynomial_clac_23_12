b4cd698aee2049339eff823d8d484ad827da00cd
eunddung <x72845221@gmail.com>
2023-12-28 13:51:21 +0900

    주석쓰는중

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,30 +2,32 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) { // 10 + (10 + 5)
-    exp = exp.trim();
-    exp = stripOuterBracket(exp);
+    exp = exp.trim();// 값을 넣었을때 그 값을 그대로 반환받을수있게함
+    exp = stripOuterBracket(exp);//괄호가 있는 exp 값
 
     // 연산기호가 없으면 바로 리턴
-    if (!exp.contains(" ")) return Integer.parseInt(exp);
-    boolean needToMultiply = exp.contains(" * ");
-    boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
-    boolean needToCompound = needToMultiply && needToPlus;
-    boolean needToSplit = exp.contains("(") || exp.contains(")");
+    if (!exp.contains(" ")) return Integer.parseInt(exp);//exp 가 공백을 포함하지않으면 Integer.parseInt(exp)을 리턴
+    boolean needToMultiply = exp.contains(" * "); //boolean 타입의 needToMultiply 변수는 "*"을 포함하면 true 못하면 false 를 반환
+    boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");//boolean 타입의 needToPlus 변수는 "+"을 포함하거나 "-"를 포함하면 true 못하면 false 를 반환
+    boolean needToCompound = needToMultiply && needToPlus;//boolean 타입의 needToCompound 변수는  needToPlu 와 needToMultiply 가 모두 true 여야 true 반환 하나라도 false 일시 false 반환
+    boolean needToSplit = exp.contains("(") || exp.contains(")");//boolean 타입의 needToSplit 변수는"("를포함하거나")"를 포함하면 true 못하면 false
 
-    if (needToSplit) {  // 800 + (10 + 5)
+    if (needToSplit) {  // 800 + (10 + 5) //needToSplit 이 true 면 실행
 
-      int splitPointIndex = findSplitPointIndex(exp);
+      int splitPointIndex = findSplitPointIndex(exp);//변수를 만든다
 
-      String firstExp = exp.substring(0, splitPointIndex);
+      String firstExp = exp.substring(0, splitPointIndex);//string 타입의 firstExp는
       String secondExp = exp.substring(splitPointIndex + 1);
 
-      char operator = exp.charAt(splitPointIndex);
+      char operator = exp.charAt(splitPointIndex);//문자형 operator 는 splitPointIndex의 위치할수있다
 
-      exp = Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp);
 
-      return Calc.run(exp);
-    } else if (needToCompound) {
-      String[] bits = exp.split(" \\+ ");
+      exp = Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp);// exp 는 Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp)
+
+      return Calc.run(exp);// Calc.run(exp)을 리턴한다
+    } else if (needToCompound) //needTosplit 이 false일때 else if 를 실행하고 needToCompound 이 true인지 확인해서 true면 아래 실행문을 실행한다
+    {
+      String[] bits = exp.split(" \\+ ");//+를 기준으로 나눈다
 
       return Integer.parseInt(bits[0]) + Calc.run(bits[1]); // TODO
     }
