6150de6a4a6b690d23c62245ba61c573c9ca17b8
eunddung <x72845221@gmail.com>
2023-12-28 15:02:24 +0900

    -(10 + 5) == -15

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,30 +1,35 @@
 package com.ll;
 
 public class Calc {
-
-  public static boolean recursionDebug = false; // 내가 디버그 모드를 켜겠다 할때는 true로 변경
+  public static boolean recursionDebug = true; // 내가 디버그 모드를 켜겠다 할때는 true로 변경
 
   public static int runCallCount = 0;
 
-  public static int run(String exp) { // 10 + (10 + 5)
+  public static int run(String exp) { // -(10 + 5)
     runCallCount++;
 
     exp = exp.trim();
     exp = stripOuterBracket(exp);
 
+    // 음수괄호 패턴이면, 기존에 갖고있던 해석 패턴과는 맞지 않으니 패턴을 변경
+    if (isNegativeCaseBracket(exp)) {
+      exp = exp.substring(1) + " * -1";
+    }
+
     if (recursionDebug) {
       System.out.printf("exp(%d) : %s\n", runCallCount, exp);
     }
-
     // 연산기호가 없으면 바로 리턴
     if (!exp.contains(" ")) return Integer.parseInt(exp);
-
     boolean needToMultiply = exp.contains(" * ");
     boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
     boolean needToCompound = needToMultiply && needToPlus;
     boolean needToSplit = exp.contains("(") || exp.contains(")");
-    if (needToSplit) {  // 800 + (10 + 5)
+
+    if (needToSplit) {  // -(10 + 5)
+
       int splitPointIndex = findSplitPointIndex(exp);
+
       String firstExp = exp.substring(0, splitPointIndex);
       String secondExp = exp.substring(splitPointIndex + 1);
       char operator = exp.charAt(splitPointIndex);
@@ -53,8 +58,32 @@ public class Calc {
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 
+  private static boolean isNegativeCaseBracket(String exp) {
+    // -로 시작하는지
+    if (exp.startsWith("-(") == false) return false;
+
+    // 괄호로 감싸져 있는지
+    int bracketCount = 0;
+
+    for (int i = 0; i < exp.length(); i++) {
+      char c = exp.charAt(i);
+
+      if (c == '(') {
+        bracketCount++;
+      } else if (c == ')') {
+        bracketCount--;
+      }
+
+      if (bracketCount == 0) {
+        if (exp.length() - 1 == i) return true;
+      }
+    }
+    return false;
+  }
+
   private static int findSplitPointIndexBy(String exp, char findChar) {
     int bracketCount = 0;
+
     for (int i = 0; i < exp.length(); i++) {
       char c = exp.charAt(i);
       if (c == '(') {
