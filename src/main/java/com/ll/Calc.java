a5d5dddc301ce10edae1388aa3da81bf3d4b3482
eunddung <x72845221@gmail.com>
2023-12-28 10:00:39 +0900

    (10 + 20) * 3

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,20 +1,18 @@
 package com.ll;
 
 public class Calc {
-  public static int run(String exp) {
+  public static int run(String exp) { // (10 + 20) * 3
     exp = exp.trim();
     exp = stripOuterBracket(exp);
 
     // 연산기호가 없으면 바로 리턴
     if (!exp.contains(" ")) return Integer.parseInt(exp);
-
     boolean needToMultiply = exp.contains(" * ");
     boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
-
     boolean needToCompound = needToMultiply && needToPlus;
     boolean needToSplit = exp.contains("(") || exp.contains(")");
 
-    if (needToSplit) {  // (20 + 20) + 20
+    if (needToSplit) {  // (10 + 20) * 3
       int bracketCount = 0;
       int splitPointIndex = -1;
 
@@ -29,15 +27,17 @@ public class Calc {
           break;
         }
       }
-
       String firstExp = exp.substring(0, splitPointIndex + 1);
       String secondExp = exp.substring(splitPointIndex + 3);
 
-      return Calc.run(firstExp) + Calc.run(secondExp);
+      char operator = exp.charAt(splitPointIndex + 2);
+
+      exp = Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp);
+
+      return Calc.run(exp);
 
     } else if (needToCompound) {
       String[] bits = exp.split(" \\+ ");
-
       return Integer.parseInt(bits[0]) + run(bits[1]); // TODO
     }
     if (needToPlus) {
@@ -58,16 +58,12 @@ public class Calc {
     }
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
-
   private static String stripOuterBracket(String exp) {
     int outerBracketCount = 0;
     while (exp.charAt(outerBracketCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketCount) == ')') {
       outerBracketCount++;
     }
-
     if (outerBracketCount == 0) return exp;
-
-
     return exp.substring(outerBracketCount, exp.length() - outerBracketCount);
   }
 }
